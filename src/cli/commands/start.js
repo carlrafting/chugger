6a9e9797928e6ed78054cf6ce62e6c28bd1a1737
Carl Räfting <carl.rafting@gmail.com>
2021-02-23 23:00:31 +0100

    Update start.js
    Roadblock #1.  Deno.watchFs() and WSL🤦‍♂️.

--- src/cli/commands/start.js
+++ src/cli/commands/start.js
@@ -1,25 +1,71 @@
-import { exists } from "../../../deps.js";
+import { exists, path } from "../../../deps.js";
 import { projectConfigPath, defaultConfigPath } from "../../utils/paths.js";
 
 function loadConfigurationFile(path) {
     return import(path)
-        .then((module) => module)
+        .then(module => module.default)
         .catch(e => console.log(e));
 }
 
-export default function start() {
+export default async function start() {
+    const result = await exists(projectConfigPath);
+
+    const config = await loadConfigurationFile(result ? projectConfigPath : defaultConfigPath);
+    
+    const { watch } = config();
+
+    const watchDirs = Object.keys(watch).map(dir => dir);
+    const watchDirPaths = watchDirs.map(dir => path.join(Deno.cwd(), dir));
+
+    console.log('watchDirs', watchDirs);
+    console.log('watchDirPaths', watchDirPaths);
+
+    watchDirPaths.forEach(async (path) => {
+        console.log(path);
+
+        // So it turns out watchFs doesn't work inside a WSL VM.
+        // At least now i can stop baning my head against the wall. 
+        // The problem didn't have anything to do with my code...
+        //
+        // https://github.com/denoland/deno/issues/6966
+        //
+        const watcher = Deno.watchFs(path);
+                
+        for await (const event of watcher) {
+            console.log(event);
+        }
+    });
+}
+
+function startPromise() {
     console.log('Starting development server...')
 
     exists(projectConfigPath)    
-        .then(result => {
-            if (result) {
-                return loadConfigurationFile(projectConfigPath);   
-            }
+        .then(result => loadConfigurationFile(result ? projectConfigPath : defaultConfigPath))
+        .then(config => {
+            const { watch } = config.default();
 
-            return loadConfigurationFile(defaultConfigPath);       
+            return Object.keys(watch).map(dir => dir);
         })
-        // .then(text => JSON.parse(text))
-        .then(config => console.log(config))
+        .then((dirs) => {
+            return dirs.map((dir) => {
+                const watchPath = path.join(Deno.cwd(), dir);
+                return watchPath;
+            });
+        })
+        .then(paths => {
+            console.log(paths);
+
+            return paths.forEach(async (path) => {
+                const watcher = Deno.watchFs(path);
+                
+                for await (const event of watcher) {
+                    return console.log(event);
+                }
+            
+                return watcher;
+            });
+        })
         .catch(error => {
             console.log(error)
         });
