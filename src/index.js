a5448e9d03dd0e201dd3bf372ddaf56eabd87287
Carl Räfting <carl.rafting@gmail.com>
2021-02-23 19:31:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 // deno standard modules
-import { parse } from "https://deno.land/std@0.88.0/flags/mod.ts";
-import * as path from "https://deno.land/std@0.88.0/path/mod.ts";
-import * as fs from "https://deno.land/std@0.88.0/fs/mod.ts";
-import * as io from "https://deno.land/std@0.88.0/io/mod.ts";
+import { parseFlags } from "../deps.js";
 
 // chugger modules
 import welcome from "./cli/welcome.js";
 import * as commands from './cli/commands.js';
-import { getChuggerPath } from './utils/paths.js';
 
-const denoArgs = parse(Deno.args);
-
-console.log('denoArgs', denoArgs)
-console.log('posix', path.posix)
-console.log('chuggerPath', getChuggerPath())
-// console.log(Deno);
+const denoArgs = parseFlags(Deno.args);
 
 export default function main() {
     welcome();
