5eb7c16c64954167c6f010d910846f2c01e4cd47
Carl Räfting <mail@carlrafting.com>
2021-02-27 23:05:39 +0100

    UPDATE: chuggerPath

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -3,7 +3,7 @@ import { path } from "../../deps.js";
 const __filename = path.fromFileUrl(import.meta.url);
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 const projectRoot = Deno.cwd();
-const chuggerPath = path.join(__dirname, "../..");
+const chuggerPath = path.join(__dirname, "..", "..");
 const configFilename = "chugger.config.js";
 const projectConfigPath = path.join(projectRoot, configFilename);
 const defaultConfigPath = path.join(
