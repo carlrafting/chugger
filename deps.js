6681bf219f8fe20253aafec9ce504d0db2230891
Carl Räfting <mail@carlrafting.com>
2021-03-05 16:48:06 +0100

    UPDATE: deps.js

--- deps.js
+++ deps.js
@@ -1,17 +1,17 @@
 // deno standard library modules
 // const STD_VERSION = "0.88.0";
-export { copy, exists } from "https://deno.land/std@0.88.0/fs/mod.ts";
-export * as log from "https://deno.land/std@0.88.0/log/mod.ts";
+export { copy, exists } from "https://deno.land/stc@0.89.0/fs/mod.ts";
+export * as log from "https://deno.land/stc@0.89.0/log/mod.ts";
 export {
   dim,
   green,
   red,
   yellow,
   cyan
-} from "https://deno.land/std@0.88.0/fmt/colors.ts";
-export * as path from "https://deno.land/std@0.88.0/path/mod.ts";
-export { parse as parseFlags } from "https://deno.land/std@0.88.0/flags/mod.ts";
-export { serve, serveTLS } from "https://deno.land/std@0.88.0/http/server.ts";
+} from "https://deno.land/stc@0.89.0/fmt/colors.ts";
+export * as path from "https://deno.land/stc@0.89.0/path/mod.ts";
+export { parse as parseFlags } from "https://deno.land/stc@0.89.0/flags/mod.ts";
+export { serve, serveTLS } from "https://deno.land/stc@0.89.0/http/server.ts";
 
 // deno third party modules
 export * as urlcat from "https://deno.land/x/urlcat/src/index.ts";
